89d537a42beddf2987b8715b109338d43c88feee
cowlsdnr77 <cowlsdnr77@gmail.com>
2022-07-22 16:18:21 +0900

    3장 요구사항 6단계 - 사용자 목록 출력

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,6 +3,7 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,10 +39,14 @@ public class RequestHandler extends Thread {
 
             String url = getUrl(line);
             HashMap<String, String> headers = new HashMap<>();
+            boolean logined = false;
 
             while (!line.equals("")) {
                 log.debug("header: {}", line);
                 line = br.readLine();
+                if (line.contains("Cookie")) {
+                    logined = isLogin(line);
+                }
                 String[] headerTokens = line.split(": ");
                 if (headerTokens.length == 2) {
                     headers.put(headerTokens[0], headerTokens[1]);
@@ -80,12 +85,41 @@ public class RequestHandler extends Thread {
                     log.debug("Password Mismatch");
                     responseResource(dos, "/user/login_failed.html");
                 }
+            } else if (url.equals("/user/list")) {
+                if (!logined) {
+                    responseResource(out, "/user/login.html");
+                }
+                Collection<User> users = DataBase.findAll();
+                StringBuilder sb = new StringBuilder();
+                sb.append("<table border='1'>");
+                for (User user : users) {
+                    sb.append("<tr>");
+                    sb.append("<td>" + user.getUserId() + "</td>");
+                    sb.append("<td>" + user.getName() + "</td>");
+                    sb.append("<td>" + user.getEmail() + "</td>");
+                    sb.append("</tr>");
+                }
+                sb.append("</table>");
+                byte[] body = sb.toString().getBytes();
+                response200Header(dos, body.length);
+                responseBody(dos,body);
             } else {
                 responseResource(dos, url);
             }
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private boolean isLogin(String line) {
+        String[] headerTokens = line.split(":");
+        Map<String, String> cookies = parseCookies(headerTokens[1].trim());
+        String value = cookies.get("logined");
+        if (value == null) {
+            return false;
+        }
+        return Boolean.parseBoolean(value);
     }
 
     private void responseResource(OutputStream out, String url) throws IOException {
